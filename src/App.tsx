d23c0e1894b952f7fc056656dc33901d98a0b16e
viamazoba <viamazoba@unal.edu.co>
2024-06-15 04:42:57 -0500

    build: Configuración base del proyecto.

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de Gastos
+        </h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <p>Formulario aquí</p>
+      </div>
+    </>
+  )
+}
+
+export default App
