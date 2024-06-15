2b6b11640d290f3ccf677a08bd943b5a04a8c179
viamazoba <viamazoba@unal.edu.co>
2024-06-15 12:31:05 -0500

    feat: Configuración de useContext y useProvider.

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,38 @@
+import { ChangeEvent, useMemo, useState } from "react"
+
+export default function BudgetForm() {
+    const [ budget, setBudget ] = useState(0)
+
+    const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
+        setBudget(+e.target.value)
+    }
+
+    const isValid = useMemo(()=>{
+        return isNaN(budget) || budget <= 0
+    }, [budget])
+    return(
+        <form action="" className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+                    Definir presupuesto
+                </label>
+                <input
+                    id="budget" 
+                    type="number"
+                    className="w-full bg-white border border-gray-200 p-2"
+                    placeholder="Define tu presupuesto"
+                    name="budget"
+                    value={budget}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <input 
+                type="submit" 
+                value='Define tu presupuesto'
+                className="bg-blue-600 hover:bg-blue-700 focus:bg-white focus:text-black focus:border-blue-600 focus:border-2 cursor-pointer w-full p-2 text-white font-black uppercase outline-none disabled:opacity-40"
+                disabled={isValid}
+            />
+        </form>
+    )
+}
